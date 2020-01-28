a33b375dccfc244936a67172b540e73411e2f4f4
Thomas <tomasmontoya123@gmail.com>
2020-01-28 13:23:16 -0500

    states typo fixed

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""States view module"""
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, jsonify
@@ -7,7 +8,7 @@ from flask import Flask, jsonify
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def states_list():
     """Retrieves the list of all State objects"""
-    sates_list = []
+    states_list = []
     states_objs = storage.all('State').values()
     for element in states_objs:
         states_list.append(element.to_dict())
