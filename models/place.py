d5837eb2b6f8af7810544189cda421019ead78cc
Cesar Velez <cavb.28@gmail.com>
2020-01-29 11:18:25 -0500

    models relationships cascade

--- models/place.py
+++ models/place.py
@@ -36,7 +36,7 @@ class Place(BaseModel, Base):
         reviews = relationship("Review", backref="place", cascade="delete")
         amenities = relationship("Amenity", secondary="place_amenity",
                                  backref="place_amenities",
-                                 viewonly=False)
+                                 viewonly=False, cascade="delete")
     else:
         city_id = ""
         user_id = ""
