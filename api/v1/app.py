47d765e057706040556cc4b8bce50df995102fe3
Cesar Velez <cavb.28@gmail.com>
2020-01-29 10:40:29 -0500

    getenv typo

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,6 +23,6 @@ def page_not_found(error):
 
 
 if __name__ == '__main__':
-    host = os.geten("HBNB_API_HOST", "0.0.0.0")
-    port = os.geten("HBNB_API_PORT", "5000")
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = os.getenv("HBNB_API_PORT", "5000")
     app.run(host=host, port=port, debug=True, threaded=True)
