0f23c5a934e294556650228c5e8d7e4d47c5c157
Cesar Velez <cavb.28@gmail.com>
2020-01-29 11:34:05 -0500

    blueprint fixed, init and index import

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''Handles flask server and blueprint'''
 from models import storage
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, Blueprint
 from api.v1.views import app_views
 import os
 
